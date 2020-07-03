5d53edac4f4f7a91f6506abe1e6987adffcc22e0
angel <angel.xie@emory.edu>
2020-07-03 15:30:18 -0400

    may 6th 2020

--- venv/jieba_tokenization.py
+++ venv/jieba_tokenization.py
@@ -2,16 +2,20 @@ import jieba
 import jieba.posseg as pseg
 import jieba.analyse as ans
 
-import csv
 
+
+import csv
+import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
-
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 
 import pyLDAvis
 import pyLDAvis.gensim
 import pyLDAvis.sklearn
+
+
 
 
 ##矫正
@@ -26,7 +30,7 @@ jieba.suggest_freq('川普', True)
 
 ##extract content column from spreadsheet
 content_list = []
-with open('TrTwAllMay06th.csv') as content:
+with open('TrTwAllCompleteMay07080910th.csv') as content:
    reader=csv.reader(content,delimiter=',')
    for row in reader:
        content_list.append(row[3])
@@ -34,18 +38,23 @@ with open('TrTwAllMay06th.csv') as content:
 content_list.remove('content')
 
 
-
+doc_length=[]
 ##tokenization (jieba)
 for i in range(len(content_list)):
     document_cut = jieba.cut(content_list[i])
     result = '/'.join(document_cut)
+
+    ##re-split the string to get doc.length
+    split_list=result.split('/')
+    doc_length.append(len(split_list))
     content_list[i]=result
 
+print(len(doc_length))
 
 ##import stopword txt
 sw_path = "stop_words.txt"
 sw_dic = open(sw_path, 'rb')
-sw_content = sw_dic.read()
+sw_content = sw_dic.read().decode('utf-8')
 #convert txt to stopwords list
 sw_list = sw_content.splitlines()
 sw_dic.close()
@@ -62,15 +71,15 @@ word_bag= cntVector.get_feature_names()
 
 
 
-lda = LatentDirichletAllocation(n_components=10,doc_topic_prior=0.001,topic_word_prior=0.8,learning_offset=50.,random_state=0)
-####docres: topic/doc distributionn
+lda = LatentDirichletAllocation(n_components=6,doc_topic_prior=0.01,topic_word_prior=5,learning_offset=50.,random_state=0)
+####docres: topic/doc distribution
 docres = lda.fit_transform(cntTf)
 
-
+for a,b in enumerate(cntTf.vocabulary_):
+    print(a)
 
 
 for topic_id,topic in enumerate(lda.components_):
-    print(topic_id)
     word_ordered_array=[]
     ### argsort sort the word(indices) in ascending order
     ###only takes the top 30 words
@@ -80,11 +89,29 @@ for topic_id,topic in enumerate(lda.components_):
 
 
 
+
+###vocab
+with open('lda_vocab.csv', "w") as my_csv:
+    csvWriter = csv.writer(my_csv)
+    my_csv.write('header')
+    my_csv.write('\n')
+    for line in word_bag:
+        my_csv.write(line)
+        my_csv.write('\n')
+
+###term frequency
+with open('lda_tf.csv', "w", newline='') as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=',')
+    csvWriter.writerows(cntTf)
+
+
+
+
 ####visualization
-data=pyLDAvis.sklearn.prepare(lda, cntTf, cntVector)
-pyLDAvis.displace(data)
+##data=pyLDAvis.sklearn.prepare(lda, cntTf, cntVector)
+#pyLDAvis.displace(data)
 #在notebook的output cell中显示
-pyLDAvis.show(data)
+#pyLDAvis.show(data)
 
 
 
--- venv/topic_modeling/jieba_tokenization_v1.py
+++ venv/topic_modeling/jieba_tokenization_v1.py
@@ -1,9 +1,7 @@
 import jieba
 import jieba.posseg as pseg
 import jieba.analyse as ans
-
 import csv
-
 import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
@@ -20,7 +18,8 @@ jieba.suggest_freq('川普', True)
 
 ##extract content column from spreadsheet
 content_list = []
-path = 'TrTwAllCompleteMay07080910th.csv'
+##path = 'TrTwAllCompleteMay07080910th.csv'
+path = 'TrTwAllCompleteMaysixth.csv'
 with open(path) as content:
    reader=csv.reader(content,delimiter=',')
    for row in reader:
@@ -61,7 +60,8 @@ tf = tf_vectorizer.fit_transform(corpus)
 tf_feature_names = tf_vectorizer.get_feature_names()
 
 
-lda = LatentDirichletAllocation(n_components=20,doc_topic_prior=0.001,topic_word_prior=0.8,learning_offset=50.,random_state=0).fit(tf)
+lda = LatentDirichletAllocation(n_components=6
+                                ,doc_topic_prior=0.1,topic_word_prior=20,learning_offset=50.,random_state=0).fit(tf)
 docres = lda.fit_transform(cntTf)
 
 def display_topics(model, feature_names, no_top_words):
@@ -72,7 +72,7 @@ def display_topics(model, feature_names, no_top_words):
         print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
 
 
-no_top_words = 20
+no_top_words = 15
 
 #print(docres)
 display_topics(lda, tf_feature_names, no_top_words)
